b2d30f3be1b73fea6980b1a0e71837b6b533bc83
the-ginsberg <jeffginsberg87@gmail.com>
2016-04-01 16:07:52 -0400

    Added Player Movement

---
+++ 2d UFO Tutorial/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody2D rb2d;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        speed = 10.0f;
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2d.AddForce(movement * speed);
+    }
+}
