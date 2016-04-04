704f920e9be084ab1f6db57f33e69a0cbc5e589f
the-ginsberg <jeffginsberg87@gmail.com>
2016-04-04 13:33:42 -0400

    Tutorial Complete, Score Tracker Added and Win Notifaction Added.

--- 2d UFO Tutorial/Assets/Scripts/PlayerController.cs
+++ 2d UFO Tutorial/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
     private Rigidbody2D rb2d;
     public float speed;
+    private int count;
+    public Text countText;
+    public Text winText;
 
 	// Use this for initialization
 	void Start () {
         speed = 10.0f;
         rb2d = GetComponent<Rigidbody2D>();
-	}
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -23,5 +30,26 @@ public class PlayerController : MonoBehaviour {
         float moveVertical = Input.GetAxis("Vertical");
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
         rb2d.AddForce(movement * speed);
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+
+    }
+
+    void SetCountText ()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You Win!";
+        }
     }
 }
